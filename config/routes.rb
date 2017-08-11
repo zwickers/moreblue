312751b5b4763f3dac710fdae650d9cfe06d9d4c
justinzwick <zwickyj@gmail.com>
2017-08-11 10:55:28 -0400

    upvote/dowvote in index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   	member do
   		put "like", to: "links#upvote"
   		put "dislike", to: "links#downvote"
+  		end
+  	end
+  	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "links#index"
 
